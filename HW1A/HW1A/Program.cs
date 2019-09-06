08a3b93cd199b5dfe9671987bb6633b2e205eccc
lupe macias <maciasg2201@uhcl.edu>
2019-09-05 21:09:02 -0500

    Added HW01

---
+++ HW1A/HW1A/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNumberOne, iNumberTwo;            //declare our working variables.
+            String sOperation;                              //declare our operation variable
+            Console.Write("Enter an integer: ");            //Ask for first number
+            iNumberOne = int.Parse(Console.ReadLine());     //Accept first number from user
+            Console.Write("Enter another integer: ");       //Ask for second number
+            iNumberTwo = int.Parse(Console.ReadLine());     //Accept second number from user
+
+            Console.Write("Enter operation ( * / + - ): ");     //Ask for operation
+            sOperation = Console.ReadLine();                       //Accept operation
+
+
+            if (sOperation == "*")
+            {
+                //multiplication
+                int iMul = iNumberOne * iNumberTwo;
+                Console.WriteLine($"Multiplying {iNumberOne} and {iNumberTwo} equals {iMul}");
+            }
+
+            if (sOperation == "/")
+            {
+                //division
+                int iDiv = iNumberOne / iNumberTwo;
+                Console.WriteLine($"Dividing {iNumberOne} by {iNumberTwo} equals {iDiv}");
+            }
+
+            if (sOperation == "+")
+            {
+                //addition
+                int iSum = iNumberOne + iNumberTwo;
+                Console.WriteLine($"Adding {iNumberOne} and {iNumberTwo} equals {iSum}");
+            }
+
+            if (sOperation == "-")
+            {
+                //subtraction
+                int iSub = iNumberOne - iNumberTwo;
+                Console.WriteLine($"Subtracting {iNumberTwo} from {iNumberOne} equals {iSub}");
+            }
+        }
+    }
+}
