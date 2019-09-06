e978df3b9c9b910b7e33045492e3c3fbf1944146
lupe macias <maciasg2201@uhcl.edu>
2019-09-05 21:23:39 -0500

    test push

---
+++ ISAM5430_Homeworks/ISAM5430_Homeworks/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISAM5430_Homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homeworks are individual projects
+        }
+    }
+}
