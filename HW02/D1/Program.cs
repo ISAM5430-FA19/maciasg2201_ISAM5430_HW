52a5e2d838a891edcf50f324cd8935d65d9cc7bf
lupe macias <maciasg2201@uhcl.edu>
2019-09-11 22:04:50 -0500

    initial upload of D

---
+++ HW02/D1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace D1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  ask use to enter a number
+            Write("Enter a count: ");
+            int iCount = Convert.ToInt32(ReadLine());
+
+            //  setup while loop counter
+            int iWhileLoopCounter = 0;
+
+            //  setup integer array, size is equal to count
+            int[] iArrNumbers = new int[iCount];
+
+            while (iWhileLoopCounter < iCount)
+            {
+                //  prompt user for that number
+                Write($"Enter integer #{iWhileLoopCounter + 1}: ");
+                iArrNumbers[iWhileLoopCounter] = Convert.ToInt32(ReadLine());
+
+                //  incremenet loop counter
+                iWhileLoopCounter++;
+            }
+            WriteLine("\n");
+        }
+    }
+}
