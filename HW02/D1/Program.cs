d22043e7f3014ca42cdc9cc3627c7b4771f3b3a3
lupe macias <maciasg2201@uhcl.edu>
2019-09-12 10:28:21 -0500

    D1: 1-6

--- HW02/D1/Program.cs
+++ HW02/D1/Program.cs
@@ -18,18 +18,56 @@ namespace D1
             //  setup while loop counter
             int iWhileLoopCounter = 0;
 
-            //  setup integer array, size is equal to count
-            int[] iArrNumbers = new int[iCount];
+            int iNumberOfPositive = 0, iNumberOfZeros = 0, iNumberOfEven = 0, iCurrentEntry, iPreviousEntry;
+            int iFirstHalfOne, iFirstHalfTwo, iSecondHalfOne, iSecondHalfTwo;
+            int iLargest = int.MinValue, iSmallest = int.MaxValue;
 
             while (iWhileLoopCounter < iCount)
             {
                 //  prompt user for that number
                 Write($"Enter integer #{iWhileLoopCounter + 1}: ");
-                iArrNumbers[iWhileLoopCounter] = Convert.ToInt32(ReadLine());
 
-                //  incremenet loop counter
+                //  accept user entry
+                iCurrentEntry = Convert.ToInt32(ReadLine());
+
+                //  check if even, zero
+                if (iCurrentEntry == 0)
+                {
+                    iNumberOfZeros++;
+                }
+                else if (iCurrentEntry % 2 == 0) {
+                    //  incremenet loop counter
+                    iNumberOfEven++;
+                }
+
+                //  check if number is positive
+                if(iCurrentEntry > 0)
+                {
+                    iNumberOfPositive++;
+                }
+
+                //  check if smallest
+                if(iCurrentEntry < iSmallest)
+                {
+                    iSmallest = iCurrentEntry;
+                }
+
+                //  check if largest
+                if(iCurrentEntry > iLargest)
+                {
+                    iLargest = iCurrentEntry;
+                }
+
+
+                //  increment loop counter
                 iWhileLoopCounter++;
             }
+            //  display results
+            WriteLine($"Number of Zeros: {iNumberOfZeros}");
+            WriteLine($"Number of Evens: {iNumberOfEven}");
+            WriteLine($"Number of Positives: {iNumberOfPositive}");
+            WriteLine($"Largest: {iLargest}");
+            WriteLine($"Smallest: {iSmallest}");
             WriteLine("\n");
         }
     }
