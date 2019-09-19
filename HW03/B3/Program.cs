e7d25d2542817825f4161e79f71435d8fcef1ce3
lupe macias <maciasg2201@uhcl.edu>
2019-09-19 14:56:39 -0500

    b3 push

---
+++ HW03/B3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace B3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  create current and largest variables
+            int iCurrent, iLargest = int.MinValue;
+
+            while (true)
+            {
+                //  prompt user to enter a number.
+                Write("B3) Enter a number: ");
+
+                //  accept user number
+                iCurrent = int.Parse(ReadLine());
+
+                //  check if if number entered is larger than previous.  if not, exit loop.
+                if(iCurrent > iLargest)
+                {
+                    //  assign current entry to largest variable
+                    iLargest = iCurrent;
+                 
+                } else
+                {
+                    WriteLine("Exiting");
+                    //  break out of the loop
+                    break;
+                }
+            }
+            //  check if a larger number was saved.
+            if(iLargest > int.MinValue)
+            {
+                //  display largest number saved.
+                WriteLine("Largest number entered = " + iLargest);
+            }
+        }
+    }
+}
