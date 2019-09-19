300888f44d4f27a3375d44774b771f974a10ca19
lupe macias <maciasg2201@uhcl.edu>
2019-09-19 18:41:12 -0500

    c10 push

---
+++ HW03/C10/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace C10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  PROMPT USER to enter a number
+            Write("Enter a number: ");
+            //  accept the number
+            int iNumber = int.Parse(ReadLine());
+
+            //  start a counter
+            for (int i = 0; i < iNumber; i++)
+            {
+                //  start another counter
+                for (int j = 0; j < iNumber; j++)
+                {
+                    //  print X
+                    Write("X");
+                }
+                //  print new line
+                Write("\n");
+            }
+        }
+    }
+}
