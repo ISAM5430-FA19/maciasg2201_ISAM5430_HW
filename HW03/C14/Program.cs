12cbf6d9c8be01b7882a588a8f599b5c26b34b34
lupe macias <maciasg2201@uhcl.edu>
2019-09-19 20:15:04 -0500

    C14  push

---
+++ HW03/C14/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace C14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  display pattern A
+            WriteLine("(a)");
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j<= i; j++)
+                {
+                    Write("*");
+                }
+                WriteLine();
+            }
+
+            //  display pattern B
+            WriteLine("(b)");
+            for(int i = 10; i > 0; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Write("*");
+                }
+                WriteLine();
+            }
+
+            //  display pattern C
+            WriteLine("(c)");
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Write(" ");
+                }
+                for(int k = 0; k < 10 -i; k++)
+                {
+                    Write("*");
+                }
+                WriteLine();
+            }
+
+            //  display pattern D
+            WriteLine("(d)");
+            for(int i = 0; i < 10; i++)
+            {
+                for(int k = 0; k < (9 - i); k++)
+                {
+                    Write(" ");
+                }
+                for(int j = 0; j <= i; j++)
+                {
+                    Write("*");
+                }
+                WriteLine();
+            }
+        }
+    }
+}
