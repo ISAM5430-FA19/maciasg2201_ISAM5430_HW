ce640439cc4967bfbeaceb7a20b309deb6c803c1
lupe macias <maciasg2201@uhcl.edu>
2019-10-06 21:29:14 -0500

    C5

---
+++ HW05/C5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace C5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date date = new Date(10, 6, 2019);
+
+            Write($"Date: {date.DisplayDate()}\n");
+
+        }
+    }
+}
